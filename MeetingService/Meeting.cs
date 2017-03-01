5c126521808eaa7600dcc95ee5b491b51c884960
Barry Stahl <barry@bsstahl.com>
2017-03-01 14:41:34 -0700

    Create meeting service and generate additional test data with it.

---
+++ MeetingService/Meeting.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeetingService
+{
+    public class Meeting
+    {
+        public int StartDay { get; set; }
+        public int NumberOfDays { get; set; }
+        public Single StartHour { get; set; }
+        public Single LengthHours { get; set; }
+        public string Location { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.StartDay},{this.NumberOfDays},{this.StartHour},{this.LengthHours},{this.Location}";
+        }
+    }
+}
---
+++ MeetingService/Service.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeetingService
+{
+    public class Service
+    {
+        const double _averageMeetingStartsPerDay = 1.5;
+        string[] _locations = new string[] { "Phoenix AZ", "Tucson AZ", "Dallas TX", "Houston TX", "New Orleans LA", "Miami FL", "New York, NY", "Albany NY" };
+
+        public IEnumerable<Meeting> GetMeetings(DateTime start, DateTime end)
+        {
+            return GenerateData(start, end);
+        }
+
+        private IEnumerable<Meeting> GenerateData(DateTime start, DateTime end)
+        {
+            var rnd = new Random();
+            var result = new List<Meeting>();
+
+            var nDays = end.Subtract(start).TotalDays;
+            for (int i = 0; i <= nDays; i++)
+            {
+                var startCount = rnd.Next(2, 4);
+                for (int j = 0; j < startCount; j++)
+                {
+                    result.Add(new Meeting()
+                    {
+                        StartDay = i + 1,
+                        NumberOfDays = rnd.Next(1, 4),
+                        StartHour = rnd.Next(9, 15),
+                        LengthHours = rnd.Next(1, 5),
+                        Location = _locations[rnd.Next(_locations.Length)]
+                    });
+                }
+            }
+
+            return result;
+        }
+
+    }
+}
