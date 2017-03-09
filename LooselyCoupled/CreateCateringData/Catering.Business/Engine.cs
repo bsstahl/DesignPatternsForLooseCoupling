fe644717f23727eed3f158d8a67a1aa872702816
bsstahl <barry@bsstahl.com>
2017-03-09 07:49:21 -0700

    Add references to Generics DI and Entities in Catering Engine.

--- LooselyCoupled/CreateCateringData/Catering.Business/Engine.cs
+++ LooselyCoupled/CreateCateringData/Catering.Business/Engine.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using Catering.Common.Extensions;
 using Catering.Common.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+using Catering.Common.Entities;
 
 namespace Catering.Business
 {
     public class Engine
     {
+        public Engine()
+        {
+        }
+
         public void CreateData()
         {
             // TODO: Retrieve the data from the repository
