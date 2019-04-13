585fecb29e3c2ec8006a36a4ccfc4ff595532874
Barry Stahl <bss_github@bsstahl.com>
2019-04-12 21:21:30 -0700

    Add missing #endregion

--- LooselyCoupled/CreateCateringData/Catering.Business/Engine.cs
+++ LooselyCoupled/CreateCateringData/Catering.Business/Engine.cs
@@ -19,29 +19,41 @@ namespace Catering.Business
 
         public void CreateData()
         {
-            // Calculate start and end dates of next month
+            #region Calculate start and end dates of next month
+
             var dataDate = DateTime.Now.AddMonths(1);
             var start = new DateTime(dataDate.Year, dataDate.Month, 1);
             var end = start.AddMonths(1).AddDays(-1);
 
-            // Retrieve the data from the repository
+            #endregion
+
+            #region Retrieve the data from the repository
+
             IMeetingRepository repo = _serviceProvider.GetService<IMeetingRepository>();
             var meetings = repo.GetMeetings(start, end);
 
-            // Determine if catering is required for any day in any meeting
+            #endregion
+
+            #region Determine if catering is required for any day in any meeting
+
             var strategy = _serviceProvider.GetService<ICateringStrategy>();
             var results = new List<CateringEvent>();
             foreach (var meeting in meetings)
             {
                 for (int i = 0; i < meeting.NumberOfDays; i++)
                 {
-                    var startDateTime = new DateTime(start.Year, start.Month, meeting.StartDay).AddDays(i).AddHours(meeting.StartHour);
+                    var startDateTime = new DateTime(start.Year, start.Month, meeting.StartDay)
+                        .AddDays(i)
+                        .AddHours(meeting.StartHour);
+
                     if (strategy.ShouldMeetingBeCatered(startDateTime, meeting.LengthHours))
                         results.Add(meeting.AsCateringEvent(start.Year, start.Month));
                 }
             }
 
-            // TODO: Output results to Catering Repository
+            #endregion
+
+            #region TODO: Output results to Catering Repository
 
             // Your mission, should you choose to accept it, is to continue the process
             // of refactoring this application by implementing an output repository.
@@ -49,6 +61,8 @@ namespace Catering.Business
             // that matches the output of the original tightly-coupled application.  If you are
             // then feeling ambitious, you could create additional repositories that output the
             // data to a database, an XML or JSON file, or even to Twitter if you feel the need.
+
+            #endregion
         }
     }
 }
