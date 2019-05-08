1f379e4b45c17fa5ade8d18c9cf90366721ed136
Barry Stahl <bss_github@bsstahl.com>
2019-05-08 01:12:29 -0700

    Add back in Strategy to container

--- LooselyCoupled/CreateCateringData/CreateCateringData/Program.cs
+++ LooselyCoupled/CreateCateringData/CreateCateringData/Program.cs
@@ -14,8 +14,17 @@ namespace CreateCateringData
         {
             // Add dependencies to container
             var container = new ServiceCollection();
+
+            // Source Data Repository - File System
+            // TODO: Retrieve the file path from arguments
             container.AddTransient<IMeetingRepository>(s => 
                 new Catering.Data.MeetingFile.Repository(@"..\..\..\..\..\..\data\April2017.csv"));
+
+            // Catering Strategy Logic
+            container.AddSingleton<ICateringStrategy>(c =>
+                new Catering.Business.Strategy.Engine());
+
+
 
             // Call the orchestration logic
             var engine = new Catering.Business.Engine(container.BuildServiceProvider());
